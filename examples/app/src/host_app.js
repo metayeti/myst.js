01e54de6f460c75ea7b79092ead8ca7290491685
pulzed <danijel.durakovic@gmail.com>
2021-03-16 21:57:12 +0100

    Add basic examples structure

---
+++ examples/app/src/host_app.js
@@ -0,0 +1,22 @@
+/*
+ * myst.js examples application
+ *
+ * (c) 2021 Danijel Durakovic
+ * MIT License
+ *
+ * host_app.js
+ * Host application features.
+ *
+ */
+
+var _host = (function() { "use strict"
+
+var _q = document.querySelector.bind(document);
+
+var public_api = {
+	
+};
+
+return public_api;
+
+}());
---
+++ examples/app/src/host_db.js
@@ -0,0 +1,23 @@
+/*
+ * myst.js examples application
+ *
+ * (c) 2021 Danijel Durakovic
+ * MIT License
+ *
+ * host_db.js
+ * Host application examples database.
+ *
+ */
+ 
+var _host_db = (function() { "use strict"
+
+var public_api = {
+	add: function(category, key, script) {
+	},
+	get: function(category, key) {
+	}
+};
+
+return public_api;
+
+}());
